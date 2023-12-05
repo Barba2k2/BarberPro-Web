e7af796db595b3053dfaf1ab1ddeacdb5bc8b8f6
Lorenzo Druziani Andreeta <lorenzo.dz@hotmail.com>
2023-12-05 09:32:07 -0300

    Started thw web system of BarbarPro

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>BarberPro - Seu Sistema Completo</title>
+      </Head>
+      <div>
+        <h1>TESTE PROJETO</h1>
+      </div>
+    </>
+  );
+}
