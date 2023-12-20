2ea2e65d981efe8666a6de14e2417f070f7f7b0b
Lorenzo Druziani Andreeta <lorenzo.dz@hotmail.com>
2023-12-20 11:54:36 -0300

    showing registred haircuts models

--- src/pages/haircuts/index.tsx
+++ src/pages/haircuts/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Head from "next/head";
 import { Sidebar } from "@/src/components/sidebar";
 import {
@@ -13,9 +14,27 @@ import {
 import Link from "next/link";
 
 import { IoMdPricetag } from "react-icons/io";
+import { canSSRAuth } from "@/src/utils/canSSRAuth";
+import { setupAPIClient } from "@/src/services/api";
 
-export default function Haircuts() {
+interface HaircutsItem {
+  id: string;
+  name: string;
+  price: number | string;
+  status: boolean;
+  user_id: string;
+}
+
+interface HaircutsProps {
+  haircuts: HaircutsItem[];
+}
+
+export default function Haircuts({ haircuts }: HaircutsProps) {
   const [isMobile] = useMediaQuery("(max-width: 500px)");
+
+  const [haircutList, setHaircutList] = useState<HaircutsItem[]>(
+    haircuts || []
+  );
 
   return (
     <>
@@ -46,7 +65,7 @@ export default function Haircuts() {
             </Heading>
 
             <Link href="/haircuts/new">
-              <Button>Cadastar Novo</Button>
+              <Button bg="barber.400" textColor="white">Cadastar Novo</Button>
             </Link>
 
             <Stack ml="auto" align="center" direction="row">
@@ -57,31 +76,80 @@ export default function Haircuts() {
             </Stack>
           </Flex>
           {/* Haircuts List */}
-          <Link href="/haircuts/123">
-            <Flex
-              cursor="pointer"
-              w={"100%"}
-              p={4}
-              bg="barber.400"
-              direction={isMobile ? "column" : "row"}
-              align={isMobile ? "flex-start" : "center"}
-              rounded="4"
-              mb={2}
-              justifyContent="space-between"
+          {haircutList.map((haircut) => (
+            <Link key={haircut.id} href={`/haircuts/${haircut.id}`}>
+              <Flex
+                cursor="pointer"
+                w={"100%"}
+                p={4}
+                bg="barber.400"
+                direction={isMobile ? "column" : "row"}
+                align={isMobile ? "flex-start" : "center"}
+                rounded="4"
+                mb={2}
+                justifyContent="space-between"
+              >
+                <Flex
+                  mb={isMobile ? 2 : 0}
+                  direction="row"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <IoMdPricetag size={28} color="#FBA931" />
+                  <Text
+                    fontWeight="bold"
+                    color="#FBA931"
+                    ml={2}
+                    mr={2}
+                    noOfLines={2}
+                  >
+                    {haircut.name}
+                  </Text>
+                </Flex>
 
-            >
-              <Flex mb={isMobile ? 2 : 0} direction="row" alignItems="center" justifyContent="center">
-                <IoMdPricetag size={28} color="#FBA931" />
-                <Text fontWeight="bold" color="#FBA931" ml={2} mr={2} noOfLines={2}>
-                  Corte Completo
+                <Text fontWeight="bold" color="white">
+                  Preço: R$ {haircut.price}
                 </Text>
               </Flex>
-
-              <Text fontWeight="bold" color="white" >Preço: R$ 59,90</Text>
-            </Flex>
-          </Link>
+            </Link>
+          ))}
         </Flex>
       </Sidebar>
     </>
   );
 }
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+  try {
+    const apiClient = setupAPIClient(ctx);
+    const response = await apiClient.get("/haircuts");
+    {
+      params: {
+        status: true;
+      }
+    }
+
+    if (response.data === null) {
+      return {
+        redirect: {
+          destination: "/dashboard",
+          permanent: false,
+        },
+      };
+    }
+
+    return {
+      props: {
+        haircuts: response.data,
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+});
